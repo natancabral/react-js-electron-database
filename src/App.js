3e8462649393614cf860f1d4a57efe0c3f6926ca
Natan Cabral <natancabral@gnial.com.br>
2021-01-25 16:13:56 -0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,7 +1,54 @@
 import React from 'react'
 //import logo from './logo.svg';
+import mysql from 'mysql';
 
 function App() {
+  
+  // set connection variable
+  const [conn,setConn] = React.useState(undefined); 
+
+  // function connection mysql remote
+  const connection = () => {
+    let c = mysql.createConnection({    
+      host     : '888.88.88.88', //:3306
+      host     : 'localhost', //:3306
+      user     : 'root',
+      password : '',
+      database : 'databasename'
+    });
+    c.connect((err) => {
+      // in case of error
+      if(err){
+          alert( err.code + "\n" + err.fatal );
+          console.log(err.code);
+          console.log(err.fatal);
+      }
+    });
+    setConn(c);
+  }
+
+  // function query/search
+  const query = () => {
+    let sql = 'SELECT `name`,`id` FROM `tablename` where id > 0  limit 0,50 ';
+    conn.query(sql, function (error, results, fields) {
+      if (error) {
+        alert(error.code);
+        console.log(error.code);
+      }
+      else {
+        alert(results);
+        console.log(results);
+        if(results.length)
+           alert(results[0].id + results[0].name);
+      }
+     });
+     
+     // Close the connection
+     connection.end(function(){
+        // The connection has been closed
+     });
+  }
+  
   return (
     <div className="App">
       <header className="App-header">
