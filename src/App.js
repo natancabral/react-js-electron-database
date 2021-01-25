571f7f996001460980c6210e62ba5cf946112e36
Natan Cabral <natancabral@gnial.com.br>
2021-01-25 15:58:20 -0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -9,14 +9,10 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    
+        <button onClick={()=>connection()}>Connection mySql</button>
+        <button onClick={()=>query()}>Query</button>
+
       </header>
     </div>
   );
