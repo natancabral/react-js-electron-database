59b496e01ff074060f2fa47dae36b7d9df9e8edc
natancabral <natancabral@gnial.com.br>
2021-09-13 12:30:27 -0300

    update

--- gulpfile.js
+++ gulpfile.js
@@ -5,42 +5,42 @@ const css = require('gulp-clean-css');
 const livereload = require('gulp-livereload');
 //1. Compile HTML file and move them to the app folder
 gulp.task('html', () => {
-    return gulp.src('src/index.html')
-        .pipe(gulp.dest('app/'))
-        .pipe(livereload());
+return gulp.src('src/index.html')
+    .pipe(gulp.dest('app/'))
+    .pipe(livereload());
 });
 
 //2. Compile CSS file and move them to the app folder
 gulp.task('css', () => {
-    return gulp.src('src/**/*.css')
-        .pipe(css())
-        .pipe(gulp.dest('app/'))
-        .pipe(livereload());
+	return gulp.src('src/**/*.css')
+		.pipe(css())
+		.pipe(gulp.dest('app/'))
+		.pipe(livereload());
 });
 
 //3. Compile JS and JSX files and move them to the app folder
 gulp.task('js*', () => {
-    return gulp.src(['main*.js', 'src/**/*.js*'])
-         .pipe(babel())
-         .pipe(gulp.dest('app/'))
-         .pipe(livereload());
+	return gulp.src(['main*.js', 'src/**/*.js*'])
+		.pipe(babel())
+		.pipe(gulp.dest('app/'))
+		.pipe(livereload());
 });
 
 //4. Compile IMAGES file and move them to the app folder
 // ------------------------------------------------------------------------------------ All images inside ./assets/
 gulp.task('images', () => {
-    return gulp.src('src/assets/**/*')
-         .pipe(gulp.dest('app/assets'))
-         .pipe(livereload());
+	return gulp.src('src/assets/**/*')
+		.pipe(gulp.dest('app/assets'))
+		.pipe(livereload());
 })
 
 //5. Watch files
 gulp.task('watch', async function() {
-  livereload.listen();
-  gulp.watch('src/**/*.html', gulp.series('html'));
-  gulp.watch('src/**/*.css', gulp.series('css'));
-  gulp.watch('src/**/*.js*', gulp.series('js*'));
-  gulp.watch('src/assets/**/*', gulp.series('images'));
+	livereload.listen();
+	gulp.watch('src/**/*.html', gulp.series('html'));
+	gulp.watch('src/**/*.css', gulp.series('css'));
+	gulp.watch('src/**/*.js*', gulp.series('js*'));
+	gulp.watch('src/assets/**/*', gulp.series('images'));
 });
 
 //6. Send to app folder
@@ -48,9 +48,9 @@ gulp.task('build', gulp.series('html', 'css', 'js*', 'images'));
 
 //7. Start the electron process.
 gulp.task('start', gulp.series('build', () => {
-    return exec(
-        __dirname+'/node_modules/.bin/electron .'
-    ).on('close', () => process.exit());
+	return exec(
+    __dirname+'/node_modules/.bin/electron .'
+).on('close', () => process.exit());
 }));
 
 //0. Default process.
